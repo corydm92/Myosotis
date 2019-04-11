e80563e0ec00ceb0ef9d806a994a3710d625d1f4
Cory <contact@corymorrissey.com>
2019-04-11 15:55:18 -0700

    got past cors issue using heroku

--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import config from "./config.json";
+import axios from "axios";
 // config.TOKEN for Trefle api key
 
 class App extends React.Component {
 	componentDidMount() {
-		fetch("https://api.example.com/items")
-			.then(res => res.json())
-			.then(
-				result => {
-					this.setState({
-						isLoaded: true,
-						items: result.items
-					});
-				},
-				// Note: it's important to handle errors here
-				// instead of a catch() block so that we don't swallow
-				// exceptions from actual bugs in components.
-				error => {
-					this.setState({
-						isLoaded: true,
-						error
-					});
+		var corsAnywhere = "https://cors-anywhere.herokuapp.com/";
+		axios
+			.get(corsAnywhere + "https://trefle.io/api/plants", {
+				headers: {
+					Authorization: "Bearer a2NscWtXRkx4dEI5UlpIQ01FWVJOdz09"
 				}
-			);
+			})
+			.then(res => console.log(res))
+			.catch(err => console.log(err));
 	}
 
 	render() {
