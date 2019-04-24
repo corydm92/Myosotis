80ce7a3707698879324475892fb86f34c2c7f507
Cory <contact@corymorrissey.com>
2019-04-24 12:41:10 -0700

    Updates

---
+++ precache-manifest.df5ff622ea631023f8a9479e304e7988.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "802bce124174a5a5d33e",
+    "url": "/corydm92/test123.git/static/js/runtime~main.1b88efbe.js"
+  },
+  {
+    "revision": "0058b8f4d4f0ae0c65d5",
+    "url": "/corydm92/test123.git/static/js/main.6b014c24.chunk.js"
+  },
+  {
+    "revision": "1ca819af5f286ff67ee3",
+    "url": "/corydm92/test123.git/static/js/2.318c083b.chunk.js"
+  },
+  {
+    "revision": "0058b8f4d4f0ae0c65d5",
+    "url": "/corydm92/test123.git/static/css/main.0c3e8ba9.chunk.css"
+  },
+  {
+    "revision": "b53d42ec129c857ec6622ed78bafe7f6",
+    "url": "/corydm92/test123.git/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/corydm92/test123/precache-manifest.33c494550d6fc83d7070fbbf2920baf4.js"
+  "/corydm92/test123.git/precache-manifest.df5ff622ea631023f8a9479e304e7988.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/corydm92/test123/index.html", {
+workbox.routing.registerNavigationRoute("/corydm92/test123.git/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
